b1bf60553a77466e8859ce21c1c665f9edbd5c93
Jorge Luis Urquiza Contreras <jlurquiza@fassil.com.bo>
2022-10-21 11:20:49 -0400

    first commit

---
+++ Application/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    //Agupar las inyecciones de nuestros servicios de terceros o propios.
+    static class ServiceExtensions
+    {
+        public static void AddAplicationLayer(this IServiceCollection service) {
+            service.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
